038dda95bba6394ef1e3a431087095eb6e1277a1
Ubed-pathan <ubedpathan818@gmail.com>
2025-10-18 17:34:05 +0530

    remove remaining JS/JSX duplicates under app

--- app/(main)/layout.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const MainLayout = async ({ children }) => {
-    // Redirect to onboarding if user is not onboarded
-  return <div className="container mx-auto mt-24 mb-20">{children}</div>;
-};
-
-export default MainLayout;
