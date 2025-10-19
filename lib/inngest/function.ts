6a2300463ffe5e76d6f99ab86e608c5afacaf610
Ubed-pathan <ubedpathan818@gmail.com>
2025-10-19 12:41:40 +0530

    added gemini 2.5 in function

--- lib/inngest/function.ts
+++ lib/inngest/function.ts
@@ -3,7 +3,7 @@ import { inngest } from "./client";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 export const generateIndustryInsights = inngest.createFunction(
   { id: "generate-industry-insights", name: "Generate Industry Insights" },
